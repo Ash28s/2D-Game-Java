228b14e783cede0e835555bc70e1be015d8ebb4a
Ash28s <125830669+Ash28s@users.noreply.github.com>
2025-06-10 00:58:43 +0530

    Added Screen and some basic game physics

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,4 +1,20 @@
 package Main;
 
+import javax.swing.*;
+
 public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("2D Adventure");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack(); // Cause this WindoW to be sized to fit the preferred size and layouts of its subcomponents (=GamePanel).
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
 }
