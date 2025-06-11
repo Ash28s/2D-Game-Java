411283e565057ffcb91f549bfe3523c5604b77d3
Ash28s <125830669+Ash28s@users.noreply.github.com>
2025-06-12 02:26:25 +0530

    Player class added

--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -1,5 +1,7 @@
 package Main;
 
+import entity.Player;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -9,7 +11,7 @@ public class GamePanel extends JPanel implements Runnable {
     final int originalTileSize = 16;
     final int scale = 3;
 
-    final int tileSize = originalTileSize * scale;
+    public final int tileSize = originalTileSize * scale;
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
     final int screenWidth = tileSize * maxScreenCol; // 768 pixels
@@ -21,6 +23,8 @@ public class GamePanel extends JPanel implements Runnable {
     KeyHandler keyH = new KeyHandler();
 
     Thread gameThread;
+
+    Player player = new Player(this, keyH);
 
     //Set Player's default position
     int playerX = 100;
@@ -100,18 +104,7 @@ public class GamePanel extends JPanel implements Runnable {
     }
 
     public void update(){
-        if (keyH.upPressed){
-            playerY -= playerSpeed;
-        }
-        else if (keyH.downPressed){
-            playerY += playerSpeed;
-        }
-        else if (keyH.leftPressed){
-            playerX -= playerSpeed;
-        }
-        else if (keyH.rightPressed){
-            playerX += playerSpeed;
-        }
+        player.update();
     }
 
     public void paintComponent(Graphics g) {            // Graphic Class:- A class that has many functions to draw objects on the screen.
@@ -119,9 +112,9 @@ public class GamePanel extends JPanel implements Runnable {
 
         Graphics2D g2 = (Graphics2D) g; //Graphics2D class extends the Graphics class to provide more sophisticated control over geometry, coordinate transformations,color management, and text layout.
 
-        g2.setColor(Color.WHITE);  // Sets a color to use for drawing objects.
+        player.draw(g2);
 
-        g2.fillRect(playerX, playerY, tileSize, tileSize); //Draw a rectangle and fills it with the specified color.
+
 
         g2.dispose();  //Dispose of this graphics context and release any system resources that it is using.
 
---
+++ src/entity/Entity.java
@@ -0,0 +1,6 @@
+package entity;
+
+public class Entity {     //This will store the variables that will be used in player, monster and NPC classes.
+    public int x, y;
+    public int speed;
+}
