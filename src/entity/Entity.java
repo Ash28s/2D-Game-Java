8a592b285662122063a1e49fe9e80e4b30d29e9a
Ash28s <125830669+Ash28s@users.noreply.github.com>
2025-06-12 03:21:50 +0530

    Player movement and its animations

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -10,4 +10,8 @@ public class Entity {     //This will store the variables that will be used in p
 
     public String direction;
 
+    public int spriteCounter = 0;
+
+    public int spriteNum = 1;
+
 }
