e3640cff6a7ec9c4bd8564dd7434e02996758f16
Ash28s <125830669+Ash28s@users.noreply.github.com>
2025-06-12 03:13:29 +0530

    Player sprites added and movement

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -1,6 +1,13 @@
 package entity;
 
+import java.awt.image.BufferedImage;
+
 public class Entity {     //This will store the variables that will be used in player, monster and NPC classes.
     public int x, y;
     public int speed;
+
+    public BufferedImage up1, down1, left1, right1, up2, down2, left2, right2;   //BufferedImage = It describes an Image with an accessible buffer of image data.(We use this to store our image files)
+
+    public String direction;
+
 }
--- src/entity/Player.java
+++ src/entity/Player.java
@@ -3,7 +3,10 @@ package entity;
 import Main.GamePanel;
 import Main.KeyHandler;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Player extends Entity{
     GamePanel gp;
@@ -14,33 +17,73 @@ public class Player extends Entity{
         this.keyH = keyH;
 
         setDefaultValues();
+        getPlayerImage();
     }
 
     public void setDefaultValues(){
         x = 100;
         y = 100;
         speed = 4;
+        direction = "down";  // Any direction is fine
+    }
+
+    public void getPlayerImage(){
+        try{
+            up1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_1.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_2.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_1.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_2.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_1.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_2.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_1.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_2.png"));
+
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
     public void update(){
         if (keyH.upPressed){
+            direction = "up";
             y -= speed;
         }
         else if (keyH.downPressed){
+            direction = "down";
             y += speed;
         }
         else if (keyH.leftPressed){
+            direction = "left";
             x -= speed;
         }
         else if (keyH.rightPressed){
+            direction = "right";
             x += speed;
         }
     }
 
     public void draw(Graphics2D g2){
 
-        g2.setColor(Color.WHITE);  // Sets a color to use for drawing objects.
+//        g2.setColor(Color.WHITE);  // Sets a color to use for drawing objects.
+//
+//        g2.fillRect(x, y, gp.tileSize, gp.tileSize); //Draw a rectangle and fills it with the specified color.
+        BufferedImage image = null;
 
-        g2.fillRect(x, y, gp.tileSize, gp.tileSize); //Draw a rectangle and fills it with the specified color.
+        switch (direction){
+            case "up":
+                image = up1;
+                break;
+            case "down":
+                image = down1;
+                break;
+            case "left":
+                image = left1;
+                break;
+            case "right":
+                image = right1;
+                break;
+        }
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
     }
 }
