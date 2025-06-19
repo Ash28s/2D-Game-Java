26a73d9c46babf782ee9d91b18e637a140bc7ef3
Ash28s <125830669+Ash28s@users.noreply.github.com>
2025-06-19 22:42:42 +0530

    Drawing Tiles and Background 3

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -4,16 +4,22 @@ import Main.GamePanel;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class TileManager {
     GamePanel gp;
     Tile[] tile;
+    int mapTileNum [][];
 
     public TileManager(GamePanel gp) {
         this.gp = gp;
         tile = new Tile[10];
+        mapTileNum = new int[gp.maxScreenCol][gp.maxScreenRow];
         getTileImage();
+        loadMap("/maps/map01.txt");
     }
 
     public void getTileImage() {
@@ -31,6 +37,36 @@ public class TileManager {
             e.printStackTrace();
         }
     }
+    public void loadMap(String filepath){
+        try{
+            InputStream is = getClass().getResourceAsStream(filepath);
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+            int col = 0;
+            int row = 0;
+
+            while (col < gp.maxScreenCol && row < gp.maxScreenRow) {
+                String line = br.readLine(); // .readLIne() = this reads the text files.
+
+                while (col < gp.maxScreenCol){     // .split(" ") = Split the string at a space.
+                    String numbers[] = line.split(" ");  //String.split(String) = Splits this string around matches of the given regular expression.
+
+                    int num = Integer.parseInt(numbers[col]);
+
+                    mapTileNum[col][row] = num;
+                    col++;
+                }
+                if (col == gp.maxScreenCol){
+                    col = 0;
+                    row++;
+                }
+            }
+            br.close();
+
+        }catch (Exception e) {
+
+        }
+    }
     public void draw(Graphics2D g2){
         int col = 0;
         int row = 0;
@@ -38,7 +74,10 @@ public class TileManager {
         int y = 0;
 
         while(col < gp.maxScreenCol && row < gp.maxScreenRow) {
-            g2.drawImage(tile[0].image, x, y,gp.tileSize, gp.tileSize, null);
+
+            int tileNum = mapTileNum[col][row];
+
+            g2.drawImage(tile[tileNum].image, x, y,gp.tileSize, gp.tileSize, null);
             col++;
             x += gp.tileSize;
 
